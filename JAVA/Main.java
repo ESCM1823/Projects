8766815858cab68e448c455cd370b4cdf04225a4
Stevens Cambranes <95002886+ESCM1823@users.noreply.github.com>
2023-07-06 18:12:07 -0600

    Add files via upload

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,11 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        //variables
-        int opc, num1, num2;
+    private static void Menu(){
 
         System.out.println("\t ---------------------------------------------- \t");
         System.out.println("\t|           Menu for basic Calculator          |\t");
@@ -17,67 +13,102 @@ public class Main {
         System.out.println("\t|                  5. EXIT                     |\t");
         System.out.println("\t|______________________________________________|\t");
         System.out.println("Choose any option you want: ");
-        //Read variable opc
-        Scanner read = new Scanner (System.in); opc = read.nextInt();
-
-        switch (opc) {
-
-            case 1 -> {
-                System.out.println(" ---------------------------------------------- ");
-                System.out.println("|                  ADDITION                    |");
-                System.out.println(" ---------------------------------------------- ");
-                System.out.println("Enter the 1st number: ");
-                num1 = read.nextInt();
-                System.out.println("Enter the 2nd number: ");
-                num2 = read.nextInt();
-                int sum = num1 + num2;
-                System.out.println("The total is: " + sum);
-            }
-
-            case 2 -> {
-                System.out.println(" ---------------------------------------------- ");
-                System.out.println("|                  SUBTRACTION                 |");
-                System.out.println(" ---------------------------------------------- ");
-                System.out.println("Enter the 1st number: ");
-                num1 = read.nextInt();
-                System.out.println("Enter the 2nd number: ");
-                num2 = read.nextInt();
-                int sub = num1 - num2;
-                System.out.println("The total is: " + sub);
-            }
-
-            case 3 -> {
-                System.out.println(" ---------------------------------------------- ");
-                System.out.println("|             MULTIPLICATION                   |");
-                System.out.println(" ---------------------------------------------- ");
-                System.out.println("Enter the 1st number: ");
-                num1 = read.nextInt();
-                System.out.println("Enter the 2nd number: ");
-                num2 = read.nextInt();
-                int mul = num1 * num2;
-                System.out.println("The total is: " + mul);
-            }
-
-            case 4 -> {
-                System.out.println(" ---------------------------------------------- ");
-                System.out.println("|                 DIVISION                     |");
-                System.out.println(" ---------------------------------------------- ");
-                System.out.println("Enter the 1st number: ");
-                num1 = read.nextInt();
-                System.out.println("Enter the 2nd number: ");
-                num2 = read.nextInt();
-                int div = num1 / num2;
-                System.out.println("The total is: " + div);
-            }
-
-            case 5 -> {
-                System.out.println(" ---------------------------------------------- ");
-                System.out.println("|        thx for use this calculator ;)        |");
-                System.out.println(" ---------------------------------------------- ");
-            }
-            default -> System.out.println("wrong choice :( try again: ");
-        }
-
     }
-    
+
+    private static void addition(){
+        //Read keyboard
+        Scanner read = new Scanner (System.in);
+
+        System.out.println(" ---------------------------------------------- ");
+        System.out.println("|                  ADDITION                    |");
+        System.out.println(" ---------------------------------------------- ");
+        System.out.println("Enter the 1st number: ");
+        int num1 = read.nextInt();
+        System.out.println("Enter the 2nd number: ");
+        int num2 = read.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The total is: " + sum);
+    }
+
+    private static void subtraction(){
+        //Read keyboard
+        Scanner read = new Scanner (System.in);
+
+        System.out.println(" ---------------------------------------------- ");
+        System.out.println("|                  SUBTRACTION                 |");
+        System.out.println(" ---------------------------------------------- ");
+        System.out.println("Enter the 1st number: ");
+        int num1 = read.nextInt();
+        System.out.println("Enter the 2nd number: ");
+        int num2 = read.nextInt();
+        int sub = num1 - num2;
+        System.out.println("The total is: " + sub);
+    }
+
+    private static void multiplication(){
+        //Read keyboard
+        Scanner read = new Scanner (System.in);
+
+        System.out.println(" ---------------------------------------------- ");
+        System.out.println("|             MULTIPLICATION                   |");
+        System.out.println(" ---------------------------------------------- ");
+        System.out.println("Enter the 1st number: ");
+        int num1 = read.nextInt();
+        System.out.println("Enter the 2nd number: ");
+        int num2 = read.nextInt();
+        int mul = num1 * num2;
+        System.out.println("The total is: " + mul);
+    }
+
+    private static void division(){
+        //Read keyboard
+        Scanner read = new Scanner (System.in);
+
+        System.out.println(" ---------------------------------------------- ");
+        System.out.println("|                 DIVISION                     |");
+        System.out.println(" ---------------------------------------------- ");
+        System.out.println("Enter the 1st number: ");
+        int num1 = read.nextInt();
+        System.out.println("Enter the 2nd number: ");
+        int num2 = read.nextInt();
+        int div = num1 / num2;
+        System.out.println("The total is: " + div);
+    }
+
+    public static void main(String[] args) {
+
+        //variables
+        int opc;
+        boolean exit = false;
+
+        //Call the function Menu
+        Menu();
+
+        //Read keyboard
+        Scanner read = new Scanner (System.in);
+
+        //make the opc variable read the input data
+        opc = read.nextInt();
+
+        while (!exit) {
+            switch (opc) {
+                case 1 -> addition();
+
+                case 2 -> subtraction();
+
+                case 3 -> multiplication();
+
+                case 4 -> division();
+
+                case 5 -> {
+                    exit = true;
+                    System.out.println(" ---------------------------------------------- ");
+                    System.out.println("|        thx for use this calculator ;)        |");
+                    System.out.println(" ---------------------------------------------- ");
+                }
+
+                default -> System.out.println("wrong choice :( try again: ");
+            }
+        }
+    }
 }
