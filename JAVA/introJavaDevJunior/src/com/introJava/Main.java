b24b40114e6125a1ecb8a2f8cea2a201b86b1c7c
ESCM1823 <cambranesstevens@gmail.com>
2024-04-28 18:17:50 -0600

    Update Kotlin & Java REP

--- JAVA/introJavaDevJunior/src/com/introJava/Main.java
+++ JAVA/introJavaDevJunior/src/com/introJava/Main.java
@@ -2,8 +2,95 @@ package com.introJava;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo, soy un Texto en JAVA");
-        
+        //Comentario de una linea
+
+        /*
+         * Comentario
+         * de
+         * Varias
+         * Lineas
+         */
+
+        System.out.print("Hola Mundo, soy un Texto sin salto de linea en JAVA");
+        System.out.println("\t\t\t\tHola Mundo, soy un Texto con salto de linea y Tabulacion en JAVA"); // \t es para hacer una tabulacion
+
+        //Variables int
+        System.out.print("\nVariables numericas con Int\n");// \n es para hacer un salto de linea
+
+        int num = 100, num1 = 100, sum, res, mul, div;
+        sum = num + num1;
+        res = num - num1;
+        mul = num * num1;
+        div = num / num1;
+
+        System.out.println("Concatenar variables: " + num + " + " + num1 + " = " + num + num1);
+        System.out.println("La sumatoria de: " + num + " + " + num1 + " = " + sum);
+        System.out.println("La resta de: " + num + " - " + num1 + " = " + res);
+        System.out.println("La multiplicacion de: " + num + " * " + num1 + " = " + mul);
+        System.out.println("La division de: " + num + " / " + num1 + " = " + div);
+
+        //Variables Double
+        System.out.print("\nVariables numericas con Double\n");// \n es para hacer un salto de linea
+
+        double numDouble = 101.55, num1Double = 100.55, sumDouble, resDouble, mulDouble, divDouble; //Double almacena 64 Bits
+        sumDouble = numDouble + num1Double;
+        resDouble = numDouble - num1Double;
+        mulDouble = numDouble * num1Double;
+        divDouble = numDouble / num1Double;
+
+        System.out.println("Concatenar variables: " + numDouble + " + " + num1Double + " = " + numDouble + num1Double);
+        System.out.println("La sumatoria de: " + numDouble + " + " + num1Double + " = " + sumDouble);
+        System.out.println("La resta de: " + numDouble + " - " + num1Double + " = " + resDouble);
+        System.out.println("La multiplicacion de: " + numDouble + " * " + num1Double + " = " + mulDouble);
+        System.out.println("La division de: " + numDouble + " / " + num1Double + " = " + divDouble);
+
+        //Variables Float
+        System.out.print("\nVariables numericas con Float\n");// \n es para hacer un salto de linea
+
+        float numFloat = 101.55f, num1Float = 100.55f, sumFloat, resFloat, mulFloat, divFloat; //Float almacena 32 Bits
+        sumFloat = numFloat + num1Float;
+        resFloat = numFloat - num1Float;
+        mulFloat = numFloat * num1Float;
+        divFloat = numFloat / num1Float;
+
+        System.out.println("Concatenar variables: " + numFloat + " + " + num1Float + " = " + numFloat + num1Float);
+        System.out.println("La sumatoria de: " + numFloat + " + " + num1Float + " = " + sumFloat);
+        System.out.println("La resta de: " + numFloat + " - " + num1Float + " = " + resFloat);
+        System.out.println("La multiplicacion de: " + numFloat + " * " + num1Float + " = " + mulFloat);
+        System.out.println("La division de: " + numFloat + " / " + num1Float + " = " + divFloat);
+
+
+        //Variables con char
+        System.out.print("\nVariables de texto con Char\n");
+
+        char inicialNombre = 'S';
+
+        System.out.println("La inicial de mi nombre es: " + inicialNombre);
+
+        //Variable con String
+        System.out.print("\nVariables de texto con String\n");
+
+        String nombre = "Stevens";
+
+        System.out.println("El nombre de la inicial: " + inicialNombre + " es: " + nombre);
+
+        //Variables mutables, inmutables y constantes
+        System.out.print("\nVariables mutables, inmutables y constantes\n");
+
+        int x = 5; // variable entera mutable
+        String nombre1 = "Edwin"; // variable de tipo String mutable
+
+        final int y = 10; // variable entera inmutable
+        final String apellido = "Cambranes"; // variable de tipo String inmutable
+
+        final double PI = 3.14159; // constante de tipo double
+        final String SALUDO = "Hola"; // constante de tipo String
+
+        System.out.println("Las variables que pueden cambiar o mutar en cualquier momento son: " + x + " y " + nombre1);
+        System.out.println("Las variables cuyo valor no puede cambiar después de haber sido asignadas por primera vez son: " + y + " y " + apellido);
+        System.out.println("Las variables cuyo valor no puede cambiar en absoluto durante la ejecución del programa son: " + PI + " y " + SALUDO);
+
+        //Lists, Sets, Maps triple S
 
     }
 }
--- KOTLIN/introKotlinDevJunior/src/com/introKotlin/Main.kt
+++ KOTLIN/introKotlinDevJunior/src/com/introKotlin/Main.kt
@@ -1,5 +1,96 @@
 package com.introKotlin
 
 fun main(){
-    println("Hola Mundo, soy un Texto en KOTLIN")
+    //Comentario de una linea
+
+    /*
+    * Comentario
+    * de
+    * Varias
+    * Lineas
+    */
+    print("Hola Mundo, soy un Texto sin salto de linea en KOTLIN")
+    println("\t\t\t\tHola Mundo, soy un Texto con salto de linea en KOTLIN")
+
+    //Variables int
+    println("\nVariables tipo Int")
+    var num:Int = 100//especificando la variable
+    var num1 = 100//sin especificar la variable
+
+    var sum:Int = num + num1
+    var res:Int = num - num1
+    var mul:Int = num * num1
+    var div:Int = num / num1
+
+    println("Concatenar variables: $num + $num1 = $num$num1")
+    println("La sumatoria de: $num + $num1 = $sum")
+    println("La resta de: $num - $num1 = $res")
+    println("La multiplicacion de: $num * $num1 = $mul")
+    println("La division de: $num / $num1 = $div")
+
+    //Variables Double
+    println("\nVariables tipo Double")
+    var numDouble:Double = 101.55
+    var num1Double:Double = 100.55
+
+    var sumDouble:Double = numDouble + num1Double
+    var resDouble:Double = numDouble - num1Double
+    var mulDouble:Double = numDouble * num1Double
+    var divDouble:Double = numDouble / num1Double
+
+    println("Concatenar variables: $numDouble + $num1Double = $numDouble$num1Double")
+    println("La sumatoria de: $numDouble + $num1Double = $sumDouble")
+    println("La resta de: $numDouble - $num1Double = $sumDouble")
+    println("La multiplicacion de: $numDouble * $num1Double = $sumDouble")
+    println("La division de: $numDouble / $num1Double = $sumDouble")
+
+    //Variables Float
+    println("\nVariables tipo Float")
+    var numFloat:Float = 101.55f
+    var num1Float:Float = 100.55f
+
+    var sumFloat:Float = numFloat + num1Float
+    var resFloat:Float = numFloat - num1Float
+    var mulFloat:Float = numFloat * num1Float
+    var divFloat:Float = numFloat / num1Float
+
+    println("Concatenar variables: $numFloat + $num1Float = $numFloat$num1Float")
+    println("La sumatoria de: $numFloat + $num1Float = $sumFloat")
+    println("La resta de: $numFloat - $num1Float = $sumFloat")
+    println("La multiplicacion de: $numFloat * $num1Float = $sumFloat")
+    println("La division de: $numFloat / $num1Float = $sumFloat")
+
+    //Variables char
+    println("\nVariables tipo Char")
+
+    var inicialNombre:Char = 'S'
+
+    println("La inicial de mi nombre es: $inicialNombre")
+
+    //Variables String
+    println("\nVariables tipo String")
+
+    var nombre:String = "Stevens"
+
+    println("El nombre de la inicial: $inicialNombre es: $nombre")
+
+    //Variables mutables e inmutables
+    println("\nVariables mutables e inmutables")
+    val popcorn = 5 //Only 5 popcorn
+    val hotdog = 7//Only 7 Hotdogs
+    var pizzaCustomers = 10
+
+    // Some customers leave the queue
+    pizzaCustomers = 8
+    println("There are $pizzaCustomers customers") // There are 8 customers
+
+    println("There are ${pizzaCustomers + 2} customers") // There are 10 customers
+    pizzaCustomers += 3
+    pizzaCustomers += 7// Example of addition
+    pizzaCustomers -= 3// Example of subtraction
+    pizzaCustomers *= 2// Example of multiplication
+    pizzaCustomers /= 3// Example of division
+
+    //Lists, Sets, Maps triple S
+
 }
